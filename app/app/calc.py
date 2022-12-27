2bba581fdaf6982f1210ba4910c20c5b4d4160bc
Carlo <carlo.nicolo@hotmail.com>
2022-12-27 09:12:34 +0100

    sample test

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+    """Add x and y return sum"""
+    return x + y
+
+def subtract(x,y):
+    """Subtract"""
+    return y - x
---
+++ app/app/test.py
@@ -0,0 +1,21 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+    
+    def test_add_numbers(self):
+        """test adding numbers together"""
+        res = calc.add(5,4)
+        
+        self.assertEqual(res,9)
+        
+    def test_substract_numbers(self):
+        """Test subtract numbers."""
+        res = calc.subtract(10,15)
+        
+        self.assertEqual(res,5)
